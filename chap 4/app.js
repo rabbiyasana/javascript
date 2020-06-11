06eae8e755ad2c19eb8a281ae1487a50fd335ad2
rabbiyasana <64980654+rabbiyasana@users.noreply.github.com>
2020-06-11 01:01:28 -0700

    Revert "Revert "javascript deploy""
    This reverts commit d6771335ccc160d0144375cf83c5957105d3ebee.

---
+++ chap 4/app.js
@@ -0,0 +1,15 @@
+//1 three variables in one statement
+var  names, age , Dept;
+//2 Declare 5 legal & 5 illegal variable names
+
+
+// illegal variables first
+// var 1name a- %ra;
+
+//legal variables
+
+var $name , _age , Id , FirstNAme;
+
+
+// 33. Display this in your browser 
+document.write("a) A heading stating “Rules for naming JS variables” <br> b)  Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable<br> c) Variables must begin with a ______, ______ or _____. For example $name, _name or name d) Variable names are case _________ <br> e) Variable names should not be JS _________"); 
